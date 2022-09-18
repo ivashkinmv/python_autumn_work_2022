608161758e19c7401ba19747199225134c3fe816
mivas <wormy99@mail.ru>
2022-09-18 19:25:52 +0300

    Домашка готова

---
+++ unit_one/task1.py
@@ -0,0 +1,23 @@
+# todo: Определить в коде  переменные:
+
+# 1. Целочисленного типа
+# 2. Вещественного типа
+# 3. Логического типа
+# 4. Строкового типа
+# 5. Пустого типа
+#
+# Вывести все их типы.
+
+# TODO: Решение:
+
+integer = 100
+divide_integer_is_float = integer/2
+boolean = bool(integer)
+one_string = str(integer)
+make_none_type = None
+
+print(type(integer))
+print(type(divide_integer_is_float))
+print(type(boolean))
+print(type(one_string))
+print(type(make_none_type))
---
+++ unit_one/task2.py
@@ -0,0 +1,24 @@
+#Преобразуйте переменную age и foo в число 
+age = "23"
+foo = "23abc"
+#Преобразуйте переменную age в Boolean
+# TODO Решение Синтактическая ошибка подкомментами:
+# age = 123abc
+#Преобразуйте переменную flag в Boolean
+flag = 1
+#Преобразуйте значение  в Boolean
+str_one = "Privet"
+str_two = ""
+#Преобразуйте значение 0 и 1  в Boolean
+
+# TODO Решение:
+
+print(int(age))
+print(int(foo[:2]))
+# print(age_is_boolean)
+print(bool(flag))
+print(bool(str_one))
+print(bool(str_two))
+print(int(False))
+print(int(True))
+
