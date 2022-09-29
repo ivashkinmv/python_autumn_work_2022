be1eaae3907b0bb4bda20f46d2b381c02a3531f6
mivas <wormy99@mail.ru>
2022-09-29 23:16:39 +0300

    Домашка НЕГОТОВА

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -10,8 +10,8 @@ mass = [1,2,17,54,30,89,2,1,6,2,0,2]
 lst1 = []
 lst2 = []
 lst3 = []
-list4 = []
-list5 = []
+# list4 = []
+# list5 = []
 list6 = []
 i = 0
 
@@ -21,20 +21,25 @@ for elem in mass:
     z = mass.count(elem)
     i += 1
     if x - y != 0:
-        list4.append(elem)
-        list5.append(x-y)
+        # list4.append(elem)
+        # list5.append(x-y)
         print(elem, x-y)
     if z > 2:
         list6.append(x)
-        print(x)
+        # print(x)
     lst1.append(x)
     lst2.append(y)
     lst3.append(z)
 
-print(mass)
-print(lst1)
-print(lst2)
-print(lst3)
-print(list4)
-print(list5)
-print(list6)
+# print(mass)
+# print(lst1)
+# print(lst2)
+# print(lst3)
+# print(list4)
+# print(list5)
+print((list6))
+# print(len(list6))
+res = 0
+while res < len(list6)-1:
+    print(list6[res+1]-list6[res])
+    res +=1
