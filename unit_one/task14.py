18207169eb7fbeeee2863cad7754dc46c64f3085
ivashkinmv <wormy99@mail.ru>
2022-09-30 20:47:29 +0300

    Домашка 3 готова без task14

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -5,13 +5,7 @@ mass = [1,2,17,54,30,89,2,1,6,2,0,2]
 # Для числа 1 минимальное растояние в массиве по индексам: 0 и 7
 # Для числа 2 минимальное растояние в массиве по индексам: 6 и 9
 # Для числа 17 нет минимального растояния т.к элемент в массиве один.
-
-
-lst1 = []
-lst2 = []
-lst3 = []
-# list4 = []
-# list5 = []
+list5 = []
 list6 = []
 i = 0
 
@@ -21,25 +15,17 @@ for elem in mass:
     z = mass.count(elem)
     i += 1
     if x - y != 0:
-        # list4.append(elem)
-        # list5.append(x-y)
         print(elem, x-y)
     if z > 2:
         list6.append(x)
-        # print(x)
-    lst1.append(x)
-    lst2.append(y)
-    lst3.append(z)
 
-# print(mass)
-# print(lst1)
-# print(lst2)
-# print(lst3)
-# print(list4)
-# print(list5)
 print((list6))
-# print(len(list6))
 res = 0
 while res < len(list6)-1:
-    print(list6[res+1]-list6[res])
+    a = list6[res+1]-list6[res]
+    list5.append(a)
+    while res < len(list5)-1:
+
+    print (a)
     res +=1
+print (list5)
