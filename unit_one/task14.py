3b812f9eb7b489872994a12913913e54711ee4c9
mivas <wormy99@mail.ru>
2022-10-01 14:45:44 +0300

    Домашка 14-ая задача готова

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -1,31 +1,34 @@
-#todo: Дан массив размера N. Найти минимальное растояние между одинаковыми значениями в массиве и вывести их индексы. 
+# TODO: Дан массив размера N. Найти минимальное растояние между одинаковыми значениями в массиве и вывести их индексы.
 
 # Пример:
-mass = [1,2,17,54,30,89,2,1,6,2,0,2]
+mass = [2, 2, 1, 2, 17, 54, 30, 89, 2, 1, 6, 2, 0, 2, 2, 54, 0]
 # Для числа 1 минимальное растояние в массиве по индексам: 0 и 7
 # Для числа 2 минимальное растояние в массиве по индексам: 6 и 9
 # Для числа 17 нет минимального растояния т.к элемент в массиве один.
-list5 = []
-list6 = []
+list1 = []
+list2 = []
+list3 = []
 i = 0
-
 for elem in mass:
     x = mass.index(elem, i, len(mass))
     y = mass.index(elem)
     z = mass.count(elem)
     i += 1
-    if x - y != 0:
-        print(elem, x-y)
+    if z == 2 and x-y > 0:
+        print("Значение элемента: ", elem, ", Наименьшее растояние между индексами: ", x-y)
     if z > 2:
-        list6.append(x)
+        list2.append(x)
+        list3.append(elem)
+cnter = 0
+while cnter < len(list2)-1:
+    a = list2[cnter+1]-list2[cnter]
+    list1.append(a)
+    cnter += 1
 
-print((list6))
-res = 0
-while res < len(list6)-1:
-    a = list6[res+1]-list6[res]
-    list5.append(a)
-    while res < len(list5)-1:
+totl = list1[0]
+for i in list1:
+    if i < totl:
+        totl = i
 
-    print (a)
-    res +=1
-print (list5)
+print("Значение элемента: ", list3[0], ", Наименьшее растояние между индексами: ", totl)
+print("Для остальных элементов нет пар и невозможно определить расстояние")
