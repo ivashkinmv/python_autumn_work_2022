4587a91f28054f90e6517089abd96fae6c2310cf
ivashkinmv <wormy99@mail.ru>
2022-10-04 15:55:11 +0300

    Task 14 нет

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -1,34 +1,34 @@
-# TODO: Дан массив размера N. Найти минимальное растояние между одинаковыми значениями в массиве и вывести их индексы.
+#todo: Дан массив размера N. Найти минимальное растояние между одинаковыми значениями в массиве и вывести их индексы. 
 
 # Пример:
-mass = [2, 2, 1, 2, 17, 54, 30, 89, 2, 1, 6, 2, 0, 2, 2, 54, 0]
+mass = [1,2,17,54,30,89,2,1,6,2,0,2]
 # Для числа 1 минимальное растояние в массиве по индексам: 0 и 7
 # Для числа 2 минимальное растояние в массиве по индексам: 6 и 9
 # Для числа 17 нет минимального растояния т.к элемент в массиве один.
-list1 = []
-list2 = []
-list3 = []
-i = 0
+# mass = [1, 1, 2, 17, 54, 30, 30, 89, 2, 1, 6, 2, 0, 2, 1, 30, 89]
+
 for elem in mass:
-    x = mass.index(elem, i, len(mass))
     y = mass.index(elem)
     z = mass.count(elem)
-    i += 1
-    if z == 2 and x-y > 0:
-        print("Значение элемента: ", elem, ", Наименьшее растояние между индексами: ", x-y)
-    if z > 2:
-        list2.append(x)
-        list3.append(elem)
-cnter = 0
-while cnter < len(list2)-1:
-    a = list2[cnter+1]-list2[cnter]
-    list1.append(a)
-    cnter += 1
-
-totl = list1[0]
-for i in list1:
-    if i < totl:
-        totl = i
-
-print("Значение элемента: ", list3[0], ", Наименьшее растояние между индексами: ", totl)
-print("Для остальных элементов нет пар и невозможно определить расстояние")
+    if z >= 2:
+        i = 0
+        indx = []
+        for dig in range(1, z+1):
+            x = mass.index(elem, i, len(mass))
+            indx.append(x)
+            i = x + 1
+        diff = []
+        res = 0
+        while res < len(indx)-1:
+            a = indx[res + 1] - indx[res]
+            # print(a)
+            diff.append(a)
+            res += 1
+        sett = {}
+        cntr = diff[0]
+        for i in diff:
+            if i < cntr:
+                cntr = i
+        print(elem, cntr, indx)
+        # if z == 1:
+        #     print ("У остальных парные элементы осутсвуют")
