6dfd15387262772a75c9c5e05f7b993ff9c4b182
ivashkinmv <wormy99@mail.ru>
2022-10-05 14:59:36 +0300

    Task 17 ГОТОВА

---
+++ unit_one/task17.py
@@ -0,0 +1,14 @@
+# todo: Создайте функцию compute_bill, считающаю итоговую сумму товаров в чеке.
+# Функция должна принимать 1 параметр - словарь, в котором указано количество едениц товара.
+# Цены хранятся в словаре:
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+def compute_bill(x):
+    s = sum(x.values())
+    return s
+
+print(compute_bill(prices))
