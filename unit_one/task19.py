b316307d95e1fd9f6d7b37c0f3885c3ed1ec0633
mivas <wormy99@mail.ru>
2022-10-05 22:01:54 +0300

    Домашка 19-ая задача готова

---
+++ unit_one/task19.py
@@ -0,0 +1,15 @@
+# todo: Требуется создать csv-файл «algoritm.csv» со следующими столбцами:
+# – id - номер по порядку (от 1 до 10);
+# – текст из списка algoritm
+
+algoritm = ["C4.5", "k - means", "Метод опорных векторов", "Apriori",
+"EM", "PageRank", "AdaBoost", "kNN", "Наивный байесовский классификатор", "CART"]
+
+# Каждое значение из списка должно находится на отдельной строке.
+
+file = open("algoritm.csv", "w")
+numbers = 1
+for i in algoritm:
+    strng = str(numbers) + ", " + str(i) + "\n"
+    numbers += 1
+    file.write(strng)
