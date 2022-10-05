d28daa87e4859511864f686488a46b04d2586cbf
ivashkinmv <wormy99@mail.ru>
2022-10-05 13:05:56 +0300

    Task 14 ГОТОВА

--- unit_one/task14.py
+++ unit_one/task14.py
@@ -1,18 +1,18 @@
-#todo: Дан массив размера N. Найти минимальное растояние между одинаковыми значениями в массиве и вывести их индексы. 
+# todo: Дан массив размера N. Найти минимальное растояние между одинаковыми значениями в массиве и вывести их индексы.
 
 # Пример:
-mass = [1,2,17,54,30,89,2,1,6,2,0,2]
-# Для числа 1 минимальное растояние в массиве по индексам: 0 и 7
-# Для числа 2 минимальное растояние в массиве по индексам: 6 и 9
+# mass = [1, 2, 17, 54, 30, 89, 2, 1, 6, 2, 0, 2]
+# Для числа 1 минимальное расcтояние в массиве по индексам: 0 и 7
+# Для числа 2 минимальное расcтояние в массиве по индексам: 6 и 9
 # Для числа 17 нет минимального растояния т.к элемент в массиве один.
-# mass = [1, 1, 2, 17, 54, 30, 30, 89, 2, 1, 6, 2, 0, 2, 1, 30, 89]
-
+mass = [1, 1, 2, 17, 54, 30, 30, 89, 2, 1, 6, 2, 0, 2, 1, 30, 89]
+mass_indx = []
 for elem in mass:
-    y = mass.index(elem)
     z = mass.count(elem)
     if z >= 2:
         i = 0
         indx = []
+        # mass_indx.append(indx)
         for dig in range(1, z+1):
             x = mass.index(elem, i, len(mass))
             indx.append(x)
@@ -24,11 +24,21 @@ for elem in mass:
             # print(a)
             diff.append(a)
             res += 1
-        sett = {}
         cntr = diff[0]
         for i in diff:
             if i < cntr:
                 cntr = i
-        print(elem, cntr, indx)
-        # if z == 1:
-        #     print ("У остальных парные элементы осутсвуют")
+        list_reply = []
+        for g in range(len(indx)-1):
+            if indx[g]+cntr == indx[g+1]:
+                a = "Для числа "
+                b = " минимальное расcтояние "
+                c = " в массиве по индексам: "
+                reply = a + str(elem) + b + str(cntr) + c + str(indx[g]) + " и " + str(indx[g + 1])
+                if reply not in mass_indx:
+                    mass_indx.append(reply)
+    if z == 1:
+        reply = ("Для числа " + str(elem) + " нет минимального растояния т.к элемент в массиве один ")
+        mass_indx.append(reply)
+for repl in mass_indx:
+    print(repl)
