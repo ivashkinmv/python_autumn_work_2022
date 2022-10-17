a83212b7e73c14a68d5a4df891c9dee21c3cf162
mivas <wormy99@mail.ru>
2022-10-17 21:06:13 +0300

    28 Задача готова

--- unit_one/task28.py
+++ unit_one/task28.py
@@ -3,3 +3,12 @@
 # todo 2: В игровой матрице nxn найти кол-во всех 1
 
 #  Задачи решить через генераторы списков (списковые включения)
+
+# TODO Задача 1
+from random import randint
+
+arr = [[randint(0, 1) for y in range(5)] for x in range(5)]
+print(arr)
+# TODO Задача 2
+arr2 = [j for i in arr for j in i].count(1)
+print(arr2)
