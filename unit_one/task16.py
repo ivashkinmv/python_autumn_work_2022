e3f1f623f3748ec460fe468ccc4d2bb9e381bd46
mivas <wormy99@mail.ru>
2022-10-21 00:12:22 +0300

    Для работы

--- unit_one/task16.py
+++ unit_one/task16.py
@@ -2,35 +2,88 @@
 # функционал в логические блоки и оформить эти блоки кода в виде функций. Стараться
 # чтобы каждая функция выполняла одно универсальное действие.
 
+
 from random import randint
+q_mstke = 10  # config Количество попыток
+def mistake():
+    global cntr
+    if cntr >= 3:
+        cntr = 0
+    cntr += 1
+    return cntr
 
 words = ["оператор", "конструкция", "объект"]
 desc_ = ["Это слово обозначает наименьшую автономную часть языка программирования", "Это конструкция загадана",
          "Это объект загадан"]
 
-q_mstke = 10  # config Количество попыток
-x = randint(0, len(words)-1) # Генератор номера задания
+x = randint(0, 2)
+print(desc_[x])
+word = words[x]
+# print(word) # Подсказка
 
-def question():
-    global x
-    print(desc_[x])
-    template = ["_"] * len(words[x])
-    string = str("".join(template)) + " Количество букв в слове:  " + str(len(words[x]))
-    return string
-print(question())
+template = ["_"] * len(word)
+print("".join(template), "Здесь", len(word), "букв")
+mstke = 0
+while word != "".join(template):
+    ans_letter = str(input("Введи букву, но только одну :"))
+    if ans_letter not in word:
+        mstke += 1
+        print("Нет такой буквы в слове", "Осталось ", q_mstke - mstke, "попыток")
+        if mstke == q_mstke:
+            print("У вас больше нет попыток")
+            break
+    cntr = 0
+    for lttr in word:
+        if ans_letter == lttr:
+            template[word.index(lttr, cntr, len(word))] = ans_letter
+            cntr = word.index(lttr, cntr, len(word)) + 1
+    print("".join(template), "Здесь", len(word), "букв")
 
-# mstke = 0
-# while word != "".join(template):
-#     ans_letter = str(input("Введи букву, но только одну :"))
-#     if ans_letter not in word:
-#         mstke += 1
-#         print("Нет такой буквы в слове", "Осталось ", q_mstke - mstke, "попыток")
-#         if mstke == q_mstke:
-#             print("У вас больше нет попыток")
-#             break
-#     cntr = 0
-#     for lttr in word:
-#         if ans_letter == lttr:
-#             template[word.index(lttr, cntr, len(word))] = ans_letter
-#             cntr = word.index(lttr, cntr, len(word)) + 1
-#     print("".join(template), "Здесь", len(word), "букв")
+a = "оператор"
+b = ["_"]*len(a)
+
+# cntr = 0
+# def mistake():
+#     global cntr
+#     global a
+#     if cntr >= len(a):
+#         cntr += 1
+#     return cntr
+# lttr = str(input("Введите букву: "))
+
+# def letter():
+#     lttr = str(input("Введите букву: "))
+#     # print(lttr)
+#     return lttr
+# # lttr = str(input("Введите букву: "))
+# cntr = 0
+# def game():
+#     global a
+#     global b
+#     global cntr
+#     lttr = str(input("Введите букву: "))
+#     if letter() in a:
+#         # x = a.index("о")
+#         x = a.index(lttr, cntr, len(a))
+#         for i in a:
+#             if lttr == i:
+#                 b[x] = lttr
+#             cntr += 1
+#         return "".join(b)
+# # print(game())
+# # def ng():
+# #     global b
+# #     global a
+# #     # global lttr
+# #     for i in a:
+# #         if letter() == i:
+# #             b[game()] = letter()
+# #     print("".join(b))
+#
+# # while
+# game()
+# # letter()
+# # ng()
+#
+#
+# # print("".join(b))
