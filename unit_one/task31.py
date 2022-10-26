41201499436288beee7faacf5a39b08ebeca6817
ivashkinmv <wormy99@mail.ru>
2022-10-26 15:03:30 +0300

    31 Готова

--- unit_one/task31.py
+++ unit_one/task31.py
@@ -1,5 +1,5 @@
-# #todo: Реализовать декоратор в котором нужно подсчитать кол-во вызовов декорированной функции в процессе выполнения кода.
-# # Выгрузить статистику подсчета в файл debug.log в формате: Название функции, кол-во вызовов, дата-время последнего выполнения
+# todo: Реализовать декоратор в котором нужно подсчитать кол-во вызовов декорированной функции в процессе выполнения кода.
+# Выгрузить статистику подсчета в файл debug.log в формате: Название функции, кол-во вызовов, дата-время последнего выполнения
 # Пример:
 # render, 10,  12.05.2022 12:00
 # show,    5,  12.05.2022 12:02
@@ -8,41 +8,43 @@
 # Декоратор должен применяться для различных функций с переменным числом аргументов.
 # Статистику вызовов необходимо записывать в файл при каждом запуске скрипта.
 
+import os
+import sys
 from time import time, strftime
 
+
 def decorator_render(func):
-    # print("Call decorator function")
-
-    def wrapper(text):
-        # print(f"Логика перед вызовом функции", strftime("%c"))
-        res = func(text)
-        # print("Логика после вызова функции", strftime("%c"))
+    def wrapper(*a):
+        f = open("debug.log", "a")
+        wrapper.count += 1
+        res = func(*a)
+        print(res[0], wrapper.count, res[2])
+        f.write(f"{res[0]} , {wrapper.count} , {res[2]} \n")
         return res
+    wrapper.count = 0
     return wrapper
-@decorator_render
-def render(text):
-    print(f"{text}", strftime("%c"))
-    menu()
-
 
 
 @decorator_render
-def show(text):
-    print(f"{text}",strftime("%c"))
-    menu()
+def render(*args):
+    return args
 
+
+@decorator_render
+def show(*args):
+    return args
 
 
 def menu():
-    input_num = int(input("Введи render или show: "))
+    input_num = int(input("Введи 1 для render или 2 для show: "))
     match input_num:
         case 1:
-            render("render")
+            render("Render", render.count+1, strftime("%c"))
+            menu()
         case 2:
-            show('show')
+            show("Show", show.count+1, strftime("%c"))
+            menu()
+        case 3:
+            sys.exit('Окончено')
 
 menu()
-
-
-
-
