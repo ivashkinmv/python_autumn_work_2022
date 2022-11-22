25bc1fcfc4660bf0daecdde49849b9fb1c1079ba
ivashkinmv <wormy99@mail.ru>
2022-11-22 18:05:08 +0300

    33 and 35, for home

--- unit_two/task33.py
+++ unit_two/task33.py
@@ -3,5 +3,72 @@
 #  2. результат запроса вернул 0 строк
 #  3. Произошел разрыв соединения с сервером
 
-a = [1,2,3]
-print(a[3])
+from random import randint
+
+dig = randint(9,10)
+ind = randint(9,10)
+l1 = [i + 1 for i in range(1, dig + 1)]
+print(l1)
+
+# class IndexIsOutOfRange(Exception):
+#     def __init__(self, number):
+#         super().__init__(f"Тра-та-та")
+#
+# try:
+#     if len(l1) < ind:
+#         raise IndexIsOutOfRange(ind)
+# except IndexIsOutOfRange(ind) as e:
+#     print(e)
+
+# class IndexIsOutOfRange(IndexError):
+#     pass
+# # def verify(digit):
+# #     if len(digit) < 2:
+# #         raise IndexIsOutOfRange("dmamdam")
+#
+# try:
+#     verify = int(input('Число: '))
+#     if verify < 2:
+#         raise IndexIsOutOfRange("dmamdam")
+#     print(verify + 2.1)
+# except IndexIsOutOfRange as e:
+#     print(e)
+#
+# class NegValException(Exception):
+#     pass
+# # def verify(digit):
+# #     if len(digit) < 2:
+# #         raise IndexIsOutOfRange("dmamdam")
+#
+# try:
+#     verify = int(input('Число: '))
+#     if verify < 0:
+#         raise NegValException("Neg val: " + str(verify))
+#     print(verify + 10)
+# except NegValException as e:
+#     print(e)
+
+
+# def validate(name):
+#     if len(name) < 6:
+#         raise ValueError
+#     else:
+#         print("Принято")
+# try:
+#     name = input("Введите имя:")
+#     validate(name)
+# except ValueError:
+#     print("Имя слишком короткое:")
+
+class NegValException(Exception):
+    def __init__(self, number):
+        super().__init__(f"Neg val: {number}")
+        self.number = number
+try:
+    val = int(input("input positive number: "))
+    if val < 0:
+        raise NegValException(val)
+except NegValException as e:
+    print(e)
+
+
--- unit_two/task35.py
+++ unit_two/task35.py
@@ -1,32 +1,58 @@
 # todo:
-1. Скачать http-clent https://insomnia.rest/download
-2. Создать новый проект и установить flask,
-3. Создать файл requirements.txt  в который занести устанавливаемые библиотеки
-4. Написать роутеры для REST сервиса/
+# 1. Скачать http-clent https://insomnia.rest/download
+# 2. Создать новый проект и установить flask,
+# 3. Создать файл requirements.txt  в который занести устанавливаемые библиотеки
+# 4. Написать роутеры для REST сервиса/
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+if __name__ == "__main__":
+    app.run()
+# from markupsafe import escape
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#
+# # show the user profile for that user
+#     return f'User {escape(username)}'
+#
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#
+# # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'
+#
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#
+# # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
     #router /student/{id}
-    def get(self, id):
-        """ Получаем студента по его id """
-        pass
-    #router /student/{id}
-    def post(self):
-        """ Передаем изменения в json """
-
-    # router /student/
-    def put(self):
-        """ Добавить студента в json """
-
-    # router /student/{id}
-    def delete(self):
-        """ Удалим студента по {id}"""
-
-
-5. Реализовать роутер /student/list - получение списка всех студентов
-студенты могут храниться как в коллекции так и в БД
-
-
-Материал для решения задачи:
-https://flask-russian-docs.readthedocs.io/ru/latest/quickstart.html#id11
+#     def get(self, id):
+#         """ Получаем студента по его id """
+#         pass
+#     #router /student/{id}
+#     def post(self):
+#         """ Передаем изменения в json """
+#
+#     # router /student/
+#     def put(self):
+#         """ Добавить студента в json """
+#
+#     # router /student/{id}
+#     def delete(self):
+#         """ Удалим студента по {id}"""
+#
+#
+# 5. Реализовать роутер /student/list - получение списка всех студентов
+# студенты могут храниться как в коллекции так и в БД
+#
+#
+# Материал для решения задачи:
+# https://flask-russian-docs.readthedocs.io/ru/latest/quickstart.html#id11
 
 
 
